5bd2e05c40cccdeafe9d53c7a960ff85073b2ff5
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-03-31 10:39:50 +0200

    added styling for the html navbar

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include_once "partials/header.php";
+?>
+
+<body>
+<nav class="bg-[#05668D] h-7 flex px-5 rounded-b-xl">
+    <div class="flex-3">
+        <img src="" alt="logo"/>
+    </div>
+    <!---->
+    <!--    <div class="flex-1">-->
+    <!--        <a href="">link 1</a>-->
+    <!--        <a href="">link 2</a>-->
+    <!--        <a href="">link 3</a>-->
+    <!--    </div>-->
+</nav>
+</body>
+<?php
+include_once "partials/footer.php";
+?>
