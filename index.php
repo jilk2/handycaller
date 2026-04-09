910288d25e21309a5ebe757d2b0556f085972151
Mariel <ruwaardmarielle@gmail.com>
2026-04-09 12:59:09 +0200

    test.html weggehaald en tekst gedeeltelijk ingevuld (ik moet eerst zorgen dat de tekst kan veranderen in interactiviteit en heb iets meer uitleg nodig over de typen sensoren voordat ik verder kan).

--- index.php
+++ index.php
@@ -19,9 +19,7 @@ include_once "partials/header.php";
     <div class="mx-12 smallSideMargin">
     <header class="bg-[#05668D] rounded-xl ">
         <p class="">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit am
+           De Handycaller
         </p>
     </header>
 
@@ -30,9 +28,11 @@ include_once "partials/header.php";
     <section id="doelgroep-en-context" class="flex flex-row rowToColumn gap-2">
         <div class="flex flex-col border-4 border-[#BDE692] flex-1 m-5 noSideMargin rounded-[40px] p-5">
             <div class=""> Doelgroep en context</div>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-            clita kasd gubergren, no sea takimata
+            Wij zijn Jilvano, Kay, Michelle en Mariëlle van team 14 en hebben samen het product 'Handycaller' gemaakt.
+            We hebben dit product gemaakt voor mensen met een motorische beperking (voornamelijk rolstoelgebruikers) en proberen winkelen voor deze groep te verbeteren.
+            Uit ons onderzoek is gebleken dat veel rolstoelgebruikers moeite hebben met hoge schappen te bereiken. Daarom moeten ze vaak om hulp vragen van medewerkers.
+            Echter is er niet altijd een medewerker in de buurt. Zo kwamen we op het idee van de Handycaller.
+            Met dit product kan je om hulp vragen van medewerkers van een afstand door ermee te schudden.
         </div>
 
         <div class="flex-1 justify-center items-center">
@@ -48,7 +48,7 @@ include_once "partials/header.php";
     <section id="type-sensor" class="flex flex-row rowToColumn gap-2">
         <div class="flex flex-col border-4 border-[#BDE692]  flex-1 m-5 noSideMargin rounded-[40px] p-3 gap-2">
             <div class="">Type sensor</div>
-            <p>Foto van het product verandert per knop...</p>
+            <p>Bij de ontwikkeling van ons product hebben we meerdere typen sensoren geprobeerd en getest. Uit deze tests kwam dat de 'schud sensor' het beste gebruikt kon worden voor de Handycaller. Door op de knoppen te klikken kunt u een foto zien van hoe dat eruitziet.</p>
             <button class="btn p-5 m-2" onclick="changeImage('images/shake.webp')">Schudden</button>
             <button class="btn p-5 m-2" onclick="changeImage('images/hover.jpg')">Hover</button>
             <button class="btn p-5 m-2" onclick="changeImage('images/press-button.jpg')">Druk knop</button>
