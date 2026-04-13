3fff941757969220a20ff34fa6b536cdecaf3877
Mariel <ruwaardmarielle@gmail.com>
2026-04-13 14:19:34 +0200

    Styling van desktop versie afgemaakt en tekst een klein beetje verandert

--- index.php
+++ index.php
@@ -5,39 +5,42 @@ include_once "partials/header.php";
 <body class="text-white">
 <div class="flex flex-col gap-[48px]">
 
-    <nav class="bg-[#05668D] items-center h-12 flex px-5 rounded-b-[80px]">
+    <nav class="bg-[#05668D] items-center h-16 flex px-5 rounded-b-[80px]">
         <div class="flex-3">
             <img src="" alt="logo"/>
         </div>
 
         <div class="flex-2 flex px-5 gap-4">
-            <a class="text-xs btn p-3" href="#" onclick="scrollToSection('doelgroep-en-context')">Doelgroep en context</a>
-            <a class="text-xs btn p-3" href="#" onclick="scrollToSection('type-sensor')">Type sensor</a>
-            <a class="text-xs btn p-3" href="#" onclick="scrollToSection('hoe-werkt-het')">Hoe werkt het?</a>
+            <a class="text-sm btn p-4" href="#" onclick="scrollToSection('doelgroep-en-context')">Doelgroep en context</a>
+            <a class="text-sm btn p-4" href="#" onclick="scrollToSection('type-sensor')">Type sensor</a>
+            <a class="text-sm btn p-4" href="#" onclick="scrollToSection('hoe-werkt-het')">Hoe werkt het?</a>
         </div>
     </nav>
     <div class="mx-12 smallSideMargin">
-    <header class="bg-[#05668D] rounded-xl ">
-        <p class="">
-           De Handycaller
-        </p>
-    </header>
-
+        <header class="bg-[#05668D] rounded-[40px] p-10 mx-5">
+            <h1 class="text-center text-5xl">
+                De Handycaller
+            </h1>
+        </header>
 
 <main class="my-12 text-black">
     <section id="doelgroep-en-context" class="flex flex-row rowToColumn gap-2">
-        <div class="flex flex-col border-4 border-[#BDE692] flex-1 m-5 noSideMargin rounded-[40px] p-5">
-            <div class=""> Doelgroep en context</div>
-            Wij zijn Jilvano, Kay, Michelle en Mariëlle van team 14 en hebben samen het product 'Handycaller' gemaakt.
-            We hebben dit product gemaakt voor mensen met een motorische beperking (voornamelijk rolstoelgebruikers) en proberen winkelen voor deze groep te verbeteren.
-            Uit ons onderzoek is gebleken dat veel rolstoelgebruikers moeite hebben met hoge schappen te bereiken. Daarom moeten ze vaak om hulp vragen van medewerkers.
-            Echter is er niet altijd een medewerker in de buurt. Zo kwamen we op het idee van de Handycaller.
-            Met dit product kan je om hulp vragen van medewerkers van een afstand door ermee te schudden.
+        <div class="flex flex-col border-4 border-[#BDE692] flex-1 m-5 noSideMargin rounded-[40px] p-5 items-center relative">
+            <h2 class="bg-[#05668D] rounded-b-[80px] rounded-t-[50px] h-14 text-white w-[101%] absolute -top-1 text-center pt-3 text-2xl">Doelgroep en context</h2>
+            <p class="mt-10 text-center align-middle text-lg">
+                Wij zijn Jilvano, Kay, Michelle en Mariëlle van team 14 en hebben samen het product 'Handycaller' gemaakt.
+                We hebben dit product gemaakt voor mensen met een motorische beperking (voornamelijk rolstoelgebruikers)
+                en proberen winkelen voor deze groep te verbeteren.
+                Uit ons onderzoek is gebleken dat veel rolstoelgebruikers moeite hebben met hoge schappen te bereiken.
+                Daarom moeten ze vaak om hulp vragen van medewerkers.
+                Echter is er niet altijd een medewerker in de buurt. Zo kwamen we op het idee van de Handycaller.
+                Met dit product kan je om hulp vragen van medewerkers van een afstand door ermee te schudden.
+            </p>
         </div>
 
         <div class="flex-1 justify-center items-center">
-            <div class="flex flex-col border border-[#BDE692] flex-1 m-5 noSideMargin">
-                <img src="images/work-in-progress.jpg" alt="a work in progress image">
+            <div class="flex flex-col border border-[#BDE692] flex-1 m-5 noSideMargin rounded-[40px]">
+                <img class="rounded-[40px]" src="images/doelgroep.png" alt="foto van doelgroep">
             </div>
         </div>
     </section>
@@ -46,21 +49,25 @@ include_once "partials/header.php";
     </div>
 
     <section id="type-sensor" class="flex flex-row rowToColumn gap-2">
-        <div class="flex flex-col border-4 border-[#BDE692]  flex-1 m-5 noSideMargin rounded-[40px] p-3 gap-2">
-            <div class="">Type sensor</div>
-            <p>Bij de ontwikkeling van ons product hebben we meerdere typen sensoren geprobeerd en getest. Deze sensoren kunt u in werking zien in de afbeelding hiernaast. Door op de knoppen te klikken kunt u een foto zien van hoe dat eruitziet.</p>
-            <button class="btn p-5 m-2" onclick="changeImage('images/shake.webp'); changeText('Je activeert de Handycaller door te schudden. Deze stuurt vervolgens een signaal naar een medewerker waarin staat dat je hulp nodig hebt in een bepaalde aisle')">Schudden</button>
+        <div class="flex flex-col border-4 border-[#BDE692]  flex-1 m-5 noSideMargin rounded-[40px] p-3 gap-2 relative">
+            <h2 class="bg-[#05668D] rounded-b-[80px] rounded-t-[50px] h-14 text-white w-[101%] absolute -top-1 left-0 text-center pt-3 text-2xl">Type sensor</h2>
+            <p class="mt-12 text-center text-lg">
+                Bij de ontwikkeling van ons product hebben we meerdere typen sensoren geprobeerd en getest.
+                Deze sensoren kunt u in werking zien in de afbeelding hiernaast.
+                Door op de knoppen te klikken kunt u een foto zien van hoe dat eruitziet.
+            </p>
+            <button class="btn p-5 m-2" onclick="changeImage('images/shake.webp'); changeText('Je activeert de Handycaller door te schudden. Deze stuurt vervolgens een signaal naar een medewerker waarin staat dat je hulp nodig hebt in een bepaalde aisle.')">Schudden</button>
             <button class="btn p-5 m-2" onclick="changeImage('images/hover.jpg'); changeText('Als je met de zender hovert over de ontvanger krijgt die met infrarood een bericht binnen.')">Hover</button>
             <button class="btn p-5 m-2" onclick="changeImage('images/press-button.jpg'); changeText('Met de knoppen op de CPX kunt u het aisle nummer veranderen zodat de medewerker je locatie weet. Dit nummer is ook gevisualiseerd met lampjes.')">Druk knop</button>
         </div>
 
         <div class="flex-2 justify-center items-center flex-col columnReverse">
-            <div class="flex flex-col border border-[#BDE692] flex-1 m-5 noSideMargin">
-                <img id="productImage" class="max-h-[50vh] transition-opacity duration-300 opacity-100" src="images/work-in-progress.jpg" alt="a work in progress image">
+            <div class="flex flex-col border border-[#BDE692] flex-1 m-5 noSideMargin rounded-[40px]">
+                <img id="productImage" class="max-h-[50vh] transition-opacity duration-300 opacity-100 rounded-[40px]" src="images/work-in-progress.jpg" alt="a work in progress image">
             </div>
-            <div id="hoe-werkt-het" class="border-4 border-[#05668D] mx-5 noSideMargin rounded-xl p-5">
-                <p id="sensor-uitleg" class="transition-opacity duration-300 opacity-100">
-                    hello
+            <div id="hoe-werkt-het" class="border-4 border-[#05668D] mx-5 noSideMargin rounded-[40px] p-5">
+                <p id="sensor-uitleg" class="transition-opacity duration-300 opacity-100 text-center">
+                    Lees hier hoe de verschillende sensoren werken.
                 </p>
             </div>
 
